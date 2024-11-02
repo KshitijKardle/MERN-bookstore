3f2451bfa6e324c4653304cba90b0e7bdec8a01f
KshitijKardile <kshitijkardile@gmail.com>
2024-11-02 10:42:45 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
-import express from "express"
-import mongoose from "mongoose"
-import {Book} from "./models/bookModel.js"
-import booksRoute from "./routes/booksRoute.js"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
-const app = express()
-const port = 3000
-const uri = "mongodb://localhost:27017/Bookstore"
+const app = express();
+const port = 3000;
+const uri = "mongodb://localhost:27017/Bookstore";
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-mongoose.connect(uri)
-.then(() => {
-    console.log('Connected to MongoDB successfully!');
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-})
-.catch((err) => {
-    console.error('Error connecting to MongoDB', err);
-})
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
 
-app.use('/books', booksRoute)
+app.use("/books", booksRoute);
